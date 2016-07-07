0c54d1f6be5ab4debb698c8f06a29fdd0a868cd0
Jeff Norton <jeff12ntn@gmail.com>
2016-07-07 13:51:58 -0600

    Implement Core Data Model section completed

---
+++ Task.swift
@@ -0,0 +1,23 @@
+//
+//  Task.swift
+//  Task
+//
+//  Created by Jeff Norton on 7/7/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+class Task: NSManagedObject {
+
+    convenience init(name: String, notes: String? = nil, dueDate: NSDate? = nil, context: NSManagedObjectContext = Stack.sharedStack.managedObjectContext) {
+        
+        let entity = NSEntityDescription.entityForName("Task", inManagedObjectContext: context)!
+        
+        self.init(entity: entity, insertIntoManagedObjectContext: context)
+        
+    }
+
+}
