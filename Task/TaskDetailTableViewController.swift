218ae173e4f456255940e8f304ab7b6827d76706
Jeff Norton <jeff12ntn@gmail.com>
2016-07-07 17:13:19 -0600

    Detail View Setup section completed

--- Task/TaskDetailTableViewController.swift
+++ Task/TaskDetailTableViewController.swift
@@ -10,43 +10,44 @@ import UIKit
 
 class TaskDetailTableViewController: UITableViewController {
 
+    // MARK: - Stored Properties
+    
+    var task: Task?
+    
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var dueDateTextField: UITextField!
+    @IBOutlet weak var notesTextView: UITextView!
+    
+    
+    // MARK: - General
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let task = task {
+            updateWith(task)
+        }
+    }
+    
+    func updateWith(task: Task) {
+        
+        taskNameTextField.text = task.name
+        dueDateTextField.text = task.dueDate?.description
+        notesTextView.text = task.notes
+        
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
@@ -81,6 +82,32 @@ class TaskDetailTableViewController: UITableViewController {
         return true
     }
     */
+    
+    // MARK: - Action(s)
+    
+    @IBAction func saveButtonTapped(sender: UIBarButtonItem) {
+        
+        guard let name = taskNameTextField.text, dueDate = dueDateTextField.text, notes = notesTextView.text where name.characters.count > 0 else { return }
+        
+        if let task = task {
+            
+            // Update existing Task
+            TaskController.sharedController.updateTask(task, name: name, notes: notes, due: NSDate(timeInterval: dueDate), isComplete: false)
+            
+        } else {
+            
+            // Save a new Task
+            TaskController.sharedController.addTask(name, notes: notes, due: dueDate)
+            
+        }
+        
+        navigationController?.popViewControllerAnimated(true)
+        
+    }
+    
+    @IBAction func cancelButtonTapped(sender: AnyObject) {
+        navigationController?.popViewControllerAnimated(true)
+    }
 
     /*
     // MARK: - Navigation
